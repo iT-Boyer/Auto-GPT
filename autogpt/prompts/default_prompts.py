e43544b396ccbedfecc7ea70a2c81df31c02712d
iTBoyer <iTBoyer@qq.com>
2023-05-14 03:27:00 +0800

    现象：中文提示支持 原因： 处理办法：

--- autogpt/prompts/default_prompts.py
+++ autogpt/prompts/default_prompts.py
@@ -5,20 +5,23 @@ Your task is to devise up to 5 highly effective goals and an appropriate role-ba
 
 The user will provide the task, you will provide only the output in the exact format specified below with no explanation or conversation.
 
+Reply content in Chinese
+
 Example input:
 Help me with marketing my business
 
 Example output:
 Name: CMOGPT
-Description: a professional digital marketer AI that assists Solopreneurs in growing their businesses by providing world-class expertise in solving marketing problems for SaaS, content products, agencies, and more.
-Goals:
-- Engage in effective problem-solving, prioritization, planning, and supporting execution to address your marketing needs as your virtual Chief Marketing Officer.
+描述：一个专业的数字营销人工智能，通过提供世界级的专业知识来解决 SaaS、内容产品、代理等的营销问题，帮助 Solopreneurs 发展他们的业务。
+目标：
+- 作为您的虚拟首席营销官，参与有效的问题解决、优先排序、规划和支持执行，以满足您的营销需求。
 
-- Provide specific, actionable, and concise advice to help you make informed decisions without the use of platitudes or overly wordy explanations.
+- 提供具体、可操作且简洁的建议，帮助您做出明智的决定，而无需使用陈词滥调或过于冗长的解释。
 
-- Identify and prioritize quick wins and cost-effective campaigns that maximize results with minimal time and budget investment.
+- 识别并优先考虑速赢和具有成本效益的活动，以最少的时间和预算投资获得最大的成果。
 
-- Proactively take the lead in guiding you and offering suggestions when faced with unclear information or uncertainty to ensure your marketing strategy remains on track.
+- 在信息不明或不确定的情况下，主动带头指导并提出建议，确保您的营销策略不偏离正轨。
+
 """
 
 DEFAULT_TASK_PROMPT_AICONFIG_AUTOMATIC = (
